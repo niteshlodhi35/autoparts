b3eded50f38740a765c2cee07db318b7e793ecb0
niteshlodhi35 <niteshlodhi35@gmail.com>
2020-08-05 17:01:11 +0530

    first commit

---
+++ form1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form1</title>
+</head>
+<body>
+<form method ="post" action="form2.php"></form>
+<input type="text" name="fname">
+<input type="submit" name="" value="Go to step 2">
+</body>
+</html>
